2c89899c7e8209996ad5ca04d43b5846baeb1c8f
Pascal Zwikirsch <pascalrene.zwikirsch@gmail.com>
2025-11-04 19:35:45 +0100

    :

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -3,7 +3,6 @@ kubectl apply -k ./gitops
 kubectl apply -f ./load-balancer
 kubectl apply -f ./metrics-server
 kubectl apply -f ./traefik
-# kubectl apply -f ./coredns
 kubectl apply -f ./rapi
 kubectl apply -f ./grpcapi
 # kubectl apply -f ./monitoring
